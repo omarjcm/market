9cd6da842f084a3f2c6dd1b1cabdb0fe3c3d59c7
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2021-02-19 01:00:32 -0500

    Estructura del Proyecto

--- src/main/java/ec/edu/ups/market/web/controller/HolaMundoController.java
+++ src/main/java/ec/edu/ups/market/web/controller/HolaMundoController.java
@@ -1,4 +1,4 @@
-package ec.edu.ups.market;
+package ec.edu.ups.market.web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
