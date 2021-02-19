d4ded3e74ca9b40cbc44fa0dd6c7d9877739c479
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2021-02-18 22:03:29 -0500

    Configuración inicial de un Proyecto de Market

---
+++ src/main/java/ec/edu/ups/market/MarketApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.ups.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ec/edu/ups/market/MarketApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.ups.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
