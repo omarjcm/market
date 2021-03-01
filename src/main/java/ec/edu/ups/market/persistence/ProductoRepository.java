c49eeb624fd9b69b94072def9c590e0352f7e3d3
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2021-03-01 01:16:00 -0500

    Implementación de la anotación @Repository.

--- src/main/java/ec/edu/ups/market/persistence/ProductoRepository.java
+++ src/main/java/ec/edu/ups/market/persistence/ProductoRepository.java
@@ -2,10 +2,12 @@ package ec.edu.ups.market.persistence;
 
 import ec.edu.ups.market.persistence.crud.ProductoCrudRepository;
 import ec.edu.ups.market.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class ProductoRepository {
 
     private ProductoCrudRepository productoCrudRepository;
@@ -24,5 +26,17 @@ public class ProductoRepository {
 
     public Optional<List<Producto>> getEscasos(int cantidad) {
         return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+    public Optional<Producto> getProducto(int idProducto) {
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto) {
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(int idProducto) {
+        productoCrudRepository.deleteById(idProducto);
     }
 }
