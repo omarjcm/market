2ed08d14c2bb56c11194bbf036359232d3a5c2ad
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2021-03-01 01:35:40 -0500

    Implementación del patrón de diseño Data Mapper.

---
+++ src/main/java/ec/edu/ups/market/domain/repository/ProductRepository.java
@@ -0,0 +1,15 @@
+package ec.edu.ups.market.domain.repository;
+
+import ec.edu.ups.market.domain.Product;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductRepository {
+    List<Product> getAll();
+    Optional<List<Product>> getByCategoryId(int categoryId);
+    Optional<List<Product>> getScarseProducts(int quantity);
+    Optional<Product> getProduct(int productId);
+    Product save(Product product);
+    void delete(int productId);
+}
